441307662c7c80db580ff87d5412b812b2452f44
a01654827@itesm.mx <nevtep123>
2020-08-20 15:24:51 -0500

    a1

--- practica1/program1.py
+++ practica1/program1.py
@@ -1,3 +1,42 @@
 ##PROGRAMA1:
+import math
+
+
+def leerNumeros(tamanoDeLista):
+    listaDeNumeros= []
+    for x in range(0,tamanoDeLista):
+        listaDeNumeros.append(float(input("InserteNumero")))
+    return listaDeNumeros
+
+def calcularMedia(lista):
+    total = 0
+    for x in lista:
+        total += x
+    #print("TOTAL =", total, "SIZE = ", len(lista))
+    media = total/len(lista)
+    return media
+
+def calcularDesviacionEstandar(lista):
+    desviacionEstandar = 0
+    media = calcularMedia(lista)
+    for x in lista:
+        desviacionEstandar += (x-media)**2
+    desviacionEstandar = desviacionEstandar/ len(lista)
+    desviacionEstandar = math.sqrt(desviacionEstandar)
+    return desviacionEstandar
+
+def programa1():
+    print("MEDIA Y DESVIACION STANDARD")
+    tamanoDeLista = (input("INSERTA TAMANO DE LISTA"))
+    tamanoDeLista = int(tamanoDeLista)
+    listaDeNumeros = leerNumeros(tamanoDeLista)
+    print(listaDeNumeros)
+    media = calcularMedia(listaDeNumeros)
+    print("MEDIA = ", media)
+    desviacionEstandar = calcularDesviacionEstandar(listaDeNumeros)
+    print("DESVIACION ESTANDAR = " , desviacionEstandar )
+
 if __name__ == '__main__':
     print("PROGRAMA 1")
+    programa1()
+
